9968668c88f201fe1bc4300f4c5b6f6f28250873
Kristo Hark <kristo.hark@kool.nrg.edu.ee>
2023-04-04 19:13:05 +0300

    liigub

--- mäng.py
+++ mäng.py
@@ -1,11 +1,85 @@
 import pygame as pg
+from random import *
 
 pg.init()
 
-screen = pg.display.set_mode((600, 600))
+#screen_w = 600
+#screen_h = 600
+#screen = pg.display.set_mode((screen_w, screen_h))
+#clock = pg.time.Clock()
+#player = pg.image.load("Pygame_logo.svg.png").convert_alpha()
+#player2 = pg.transform.scale(player, (200,200))
+
+def bouncing_rect():
+    global x_speed, y_speed, other_speed, other_speed2, other_speed3
+    moving_rect.x += x_speed
+    moving_rect.y += y_speed
+
+    if moving_rect.right >= screen_width or moving_rect.left <= 0:
+        x_speed *= -1
+    if moving_rect.bottom >= screen_height or moving_rect.top <= 0:
+        y_speed *= -1
+
+    other_rect.y += other_speed
+    if other_rect.top <= 0 or other_rect.bottom >= screen_height:
+        other_speed *= -1
+
+    other_rect2.x += other_speed2
+    if other_rect2.left <= 0 or other_rect2.right >= screen_width:
+        other_speed2 *= -1
+
+    other_rect3.y += other_speed3
+    if other_rect3.top <= 0 or other_rect3.bottom >= screen_height:
+        other_speed3 *= -1
+
+    collision_tolerance = 10
+    if moving_rect.colliderect(other_rect):
+        if abs(other_rect.top - moving_rect.bottom) < collision_tolerance and y_speed > 0:
+            y_speed *= -1
+        if abs(other_rect.bottom - moving_rect.top) < collision_tolerance and y_speed < 0:
+            y_speed *= -1
+        if abs(other_rect.bottom - moving_rect.left) < collision_tolerance and x_speed < 0:
+            x_speed *= -1
+        if abs(other_rect.bottom - moving_rect.right) < collision_tolerance and x_speed > 0:
+            x_speed *= -1
+    if moving_rect.colliderect(other_rect2):
+        if abs(other_rect2.top - moving_rect.bottom) < collision_tolerance and y_speed > 0:
+            y_speed *= -1
+        if abs(other_rect2.bottom - moving_rect.top) < collision_tolerance and y_speed < 0:
+            y_speed *= -1
+        if abs(other_rect2.bottom - moving_rect.left) < collision_tolerance and x_speed < 0:
+            x_speed *= -1
+        if abs(other_rect2.bottom - moving_rect.right) < collision_tolerance and x_speed > 0:
+            x_speed *= -1
+    if moving_rect.colliderect(other_rect3):
+        if abs(other_rect3.top - moving_rect.bottom) < collision_tolerance and y_speed > 0:
+            y_speed *= -1
+        if abs(other_rect3.bottom - moving_rect.top) < collision_tolerance and y_speed < 0:
+            y_speed *= -1
+        if abs(other_rect3.bottom - moving_rect.left) < collision_tolerance and x_speed < 0:
+            x_speed *= -1
+        if abs(other_rect3.bottom - moving_rect.right) < collision_tolerance and x_speed > 0:
+            x_speed *= -1
+    pg.draw.rect(screen, (255, 0, 0), moving_rect)
+    pg.draw.rect(screen, (0, 255, 0), other_rect)
+    pg.draw.rect(screen, (0, 0, 255), other_rect2)
+    pg.draw.rect(screen, (255, 0, 255), other_rect3)
+
+pg.init()
 clock = pg.time.Clock()
-player = pg.image.load("Pygame_logo.svg.png").convert_alpha()
-player2 = pg.transform.scale(player, (200,200))
+screen_width, screen_height = 600, 600
+screen = pg.display.set_mode((screen_width, screen_height))
+moving_rect = pg.Rect(350, 350, 100, 100)
+x_speed, y_speed = 5, 4
+
+other_rect = pg.Rect(300, 400, 200, 100)
+other_speed = 2
+
+other_rect2 = pg.Rect(randint(100, 300), randint(100, 400), randint(100, 200), randint(50, 100))
+other_speed2 = randint(1, 10)
+
+other_rect3 = pg.Rect(randint(100, 300), randint(100, 400), randint(100, 200), randint(50, 100))
+other_speed3 = randint(2, 10)
 
 class Ruut:
     def __init__(self, kyljepikkus):
@@ -102,5 +176,9 @@ while gameplay:
     pg.display.update()
     #screen.blit(player2, (x, y))
 
-    pg.display.update()
+    """pg.display.update()
+    clock.tick(60)"""
+
+    bouncing_rect()
+    pg.display.flip()
     clock.tick(60)
